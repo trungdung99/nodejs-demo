4142626a610e8030988b952b373ea9914603b946
Trung Dung <dungptph14123@fpt.edu.vn>
2022-03-10 16:49:19 +0700

    update instal express

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 // Bước 1: include thư viện http
 const http = require('http');
+const express = require('express');
+const { response } = require('express');
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (request, response) => {
+    response.send(`
+        <h1>Home Page</h1>
+    `);
+});
+app.get('/products', (request, response) => {
+    response.send(`
+        <h1>Product Page</h1>
+    `)
+});
+app.get('/news', (request, response) => {
+    response.send(`
+        <h1>News Page</h1>
+    `)
+});
 
 // Bước 2: khởi tạo server
-const server = http.createServer((request, response) =>{
-    //console.log(request.url);
-    if (request.url === '/'){
-        response.setHeader("Content-Type","text/html");
-        response.write("<h1>Home Page</h1>")
-        response.end();
-    }
-    if (request.url === '/products'){
-        response.setHeader("Content-Type","text/html");
-        response.write("<h1>Product Page</h1>")
-        response.end();
-    }
-    if (request.url === '/news'){
-        response.setHeader("Content-Type","text/html");
-        response.write("<h1>News Page</h1>")
-        response.end();
-    }
-});
+// const server = http.createServer((request, response) =>{
+//     //console.log(request.url);
+//     if (request.url === '/'){
+//         response.setHeader("Content-Type","text/html");
+//         response.write("<h1>Home Page</h1>")
+//         response.end();
+//     }
+//     if (request.url === '/products'){
+//         response.setHeader("Content-Type","text/html");
+//         response.write("<h1>Product Page</h1>")
+//         response.end();
+//     }
+//     if (request.url === '/news'){
+//         response.setHeader("Content-Type","text/html");
+//         response.write("<h1>News Page</h1>")
+//         response.end();
+//     }
+// });
 
 // Bước 3: lắng nghe cổng thực thi 
 const port = 3001;
